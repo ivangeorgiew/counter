1c2935d7abbfe175eedc0138d35091f117de0aef
Ivan Georgiev <ivan_georgiew@yahoo.com>
2020-08-25 12:49:19 +0300

    Updates

---
+++ precache-manifest.b5f041b83460e9922bef3e223cbf2015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495b4c2a674dcaa46088dddbec737fd1",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "9a39cf9cd6ddaf3c7fed",
+    "url": "/counter/static/css/main.c2d3ff29.chunk.css"
+  },
+  {
+    "revision": "ea45d64e5a7b54fe2bae",
+    "url": "/counter/static/js/2.f6b8d7f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.f6b8d7f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a39cf9cd6ddaf3c7fed",
+    "url": "/counter/static/js/main.70e289d2.chunk.js"
+  },
+  {
+    "revision": "f0efe914179fd241fe39",
+    "url": "/counter/static/js/runtime-main.60e3ae63.js"
+  }
+]);
